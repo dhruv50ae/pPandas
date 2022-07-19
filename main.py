c9b28982d4e399780be26d52e1a643d475865f34
dhruv50ae <linkdp2012@gmail.com>
2022-07-19 12:19:09 +0100

    Use panda library

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import pandas as pan
 
 data = pan.read_csv("weather-data.csv")
-print(data["temp"])
+
+dataDict = {
+    "students" : ["Alpha", "Beta", "Charlie"],
+    "scores" : [76, 56, 65]
+}
+dataF = pan.DataFrame(dataDict)
+
+dataF.to_csv("newData.csv")
